ffcbc223687fcdf612f3b1e8631d4e37c8534e1b
Raphael Marco <raphaelmarco@outlook.com>
2020-07-31 20:22:35 +0800

    Add docblock to ioredis

--- services/ioredis.js
+++ services/ioredis.js
@@ -4,6 +4,11 @@ const { url } = require('@/config/redis');
 
 let instance = null;
 
+/**
+ * Get ioredis instance
+ *
+ * @return {Redis}
+ */
 exports.getRedisInstance = () => {
   if (instance === null) {
     instance = new Redis(url);
